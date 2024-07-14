fe52abbd9afef3d966da4268896e20500fab9ad4
Nick Rabone <n.rabone72@aol.com>
2024-07-14 17:35:26 +0100

    Project completed

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
 camera.position.z = 1.5
 
 const renderer = new THREE.WebGLRenderer()
