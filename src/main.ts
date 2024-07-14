aa9cc8af31ff40f3a94c3ef955be6d0ec835dd07
Nick Rabone <n.rabone72@aol.com>
2024-07-14 17:28:06 +0100

    Controlled rendering

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
@@ -16,9 +15,13 @@ window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.render(scene, camera) 
 })
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', () => {
+  renderer.render(scene, camera)
+})
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
@@ -26,23 +29,4 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-const stats = new Stats()
-document.body.appendChild(stats.dom)
-
-const clock = new THREE.Clock()
-let delta
-
-function animate() {
-  requestAnimationFrame(animate)
-
-  delta = clock.getDelta()
-
-  cube.rotation.x += delta
-  cube.rotation.y += delta
-
-  renderer.render(scene, camera)
-
-  stats.update()
-}
-
-animate()
+renderer.render(scene, camera) 
